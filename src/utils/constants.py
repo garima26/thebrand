5b8bb8de6c8c75c36a04d347189a693814e115cf
Garima Jain <garima.jain@externals.adidas.com>
2021-11-13 22:41:41 +0100

    Run first model

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,30 +1,10 @@
 from pathlib import Path
 
-BASE_DIR = Path("/")
-DATA_PATH = BASE_DIR / "data"
-ARTIFACT_PATH = BASE_DIR / "artifacts"
-
-MODEL_FEATURES = ['single',
-                 'married_no_kids',
-                 'not_married_no_kids',
-                 'married_with_kids',
-                 'not_married_with_kids',
-                 'single_parent',
-                 'other',
-                 'perc_area_single',
-                 'perc_area_married_no_kids',
-                 'perc_area_not_married_no_kids',
-                 'perc_area_married_with_kids',
-                 'perc_area_not_married_with_kids',
-                 'perc_area_single_parent',
-                 'perc_area_other',
-                 'Highest_income_hhs',
-                 'Touristy',
-                 'Diversity',
-                 'HasHomeStadium',
-                 'IsPosh',
-                 'total',
-                 'area_has_buurt',
-                 'borough']
+MODEL_FEATURES = ['MATERIAL_CONTENT_CODE',
+                  'PRODUCT_TYPES',
+                  'BUSINESS_SEGMENTS',
+                  'Sustainablity',
+                  'MARKETING_CATEGORY',
+                  'nrf_color_group']
 
 TARGET = 'Units_sold'
